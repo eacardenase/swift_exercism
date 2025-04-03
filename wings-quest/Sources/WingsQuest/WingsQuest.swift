16a370af3a56cebeb0c6737c882c381b907ac7b7
Edwin Cardenas <ecardenas@lwolf.com>
2025-04-02 21:17:39 -0500

    refactor: code cleanup

--- wings-quest/Sources/WingsQuest/WingsQuest.swift
+++ wings-quest/Sources/WingsQuest/WingsQuest.swift
@@ -1,31 +1,15 @@
 func bonusPoints(powerUpActive: Bool, touchingEagle: Bool) -> Bool {
-  if powerUpActive && touchingEagle {
-    return true
-  }
-
-  return false
+  powerUpActive && touchingEagle
 }
 
 func score(touchingPowerUp: Bool, touchingSeed: Bool) -> Bool {
-  if touchingPowerUp || touchingSeed {
-    return true
-  }
-
-  return false
+  touchingPowerUp || touchingSeed
 }
 
 func lose(powerUpActive: Bool, touchingEagle: Bool) -> Bool {
-  if touchingEagle && !powerUpActive {
-    return true
-  }
-
-  return false
+  touchingEagle && !powerUpActive
 }
 
 func win(hasPickedUpAllSeeds: Bool, powerUpActive: Bool, touchingEagle: Bool) -> Bool {
-  if lose(powerUpActive: powerUpActive, touchingEagle: touchingEagle) {
-    return false
-  }
-
-  return true
+  hasPickedUpAllSeeds && !lose(powerUpActive: powerUpActive, touchingEagle: touchingEagle)
 }
